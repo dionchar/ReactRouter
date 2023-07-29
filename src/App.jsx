2b71e2df3aa2293e9db039e437ad9e55ba25372d
Dion Charles <dionr_charles@hotmail.com>
2023-07-29 19:37:14 -0400

    new files

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  const [state, setState] = useState();
+  
+  return (
+
+<div id="container">
+      <h1></h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
