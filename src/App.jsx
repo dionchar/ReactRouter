62f3deb4a0c2333375f6adfeb192bdc84da78cf6
Dion Charles <dionr_charles@hotmail.com>
2023-07-29 20:20:12 -0400

    last changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
-import Red from './Red.jsx';
-import Blue from './Blue.jsx';
-import './Home.jsx';
+import Red from './components/Red.jsx';
+import Blue from './components/Blue.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   const [state, setState] = useState();
@@ -15,14 +15,14 @@ function App() {
 <div id="container">
       <h1></h1>
       <div id="navbar">
-        <Link to="/blue">Blue</Link>
-        <Link to="/red">Red</Link>
+        <Link to="/Blue">Blue</Link>
+        <Link to="/Red">Red</Link>
         <Link to="/">Home</Link>
       </div>
       <div id="main-section">
         <Routes>
-          <Route path="blue" element={<Blue/>} />
-          <Route path="red" element={<Red/>} />
+          <Route path="/Blue" element={<Blue/>} />
+          <Route path="/Red" element={<Red/>} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
@@ -30,28 +30,4 @@ function App() {
   );
 }
 
-
-
-
-
-
-
-
-
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
